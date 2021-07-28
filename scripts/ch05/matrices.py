24f275370ca6f1b1a74061bd3fa9450771380486
Milan Ivošević <miv@spyro-soft.com>
2021-07-28 14:21:13 +0200

    Excercises up to 5.10

--- scripts/ch05/matrices.py
+++ scripts/ch05/matrices.py
@@ -5,6 +5,17 @@ def multiply_matrix_vector(m, v):
     return linear_combination(v, *zip(*m))
 
 
+def multiply_matrix_vector2(m, v):
+    return tuple(
+            sum(row_elem*vector_elem for row_elem, vector_elem in zip(row, v)) 
+            for row in m
+        )
+
+
+def multiply_matrix_vector3(m, v):
+    return tuple(dot(row, v) for row in m)
+
+
 def matrix_multiply(m1, m2):
     m2_columns = tuple(zip(*m2))
 
