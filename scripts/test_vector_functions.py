8a775699c147d1feeddd1e2138ba7dd3af048d5b
Milan Ivošević <mivos6@gmail.com>
2021-08-20 08:24:06 +0200

    Readable tests using composite

--- scripts/test_vector_functions.py
+++ scripts/test_vector_functions.py
@@ -1,102 +1,117 @@
-from hypothesis import given, strategies as st
+from hypothesis import given
+import hypothesis.strategies as strats
+from hypothesis.strategies import composite
 import pytest
 from .vectors import add, subtract, distance, dot, angle, cross, projection_length, project_vectors_to_xy_plane, linear_combination
 
-
-scalars = st.floats(min_value=-1000, max_value=1000, allow_nan=False)
-
-# generate variable-length vectors
-vectors = st.integers(min_value=2, max_value=10, ).flatmap(
-    lambda n: st.tuples(*[scalars for _ in range(n)]))
-
-lists_of_vectors_of_unique_length = st.lists(
-    vectors, min_size=2, max_size=100, unique_by=lambda v: len(v))
+scalar = strats.floats(min_value=-1000, max_value=1000, allow_nan=False)
 
 
-@given(vl=lists_of_vectors_of_unique_length)
+def length(min_val=0, max_val=None):
+    return strats.integers(min_value=min_val, max_value=max_val)
+
+
+@composite
+def vector(draw, min_size=0, max_size=None):
+    vector_size = draw(length(min_size, max_size))
+    vectors = strats.tuples(*[scalar for _ in range(vector_size)])
+    return draw(vectors)
+
+
+def list_of_vectors(vector_strat=vector(max_size=20), min_length=0, max_length=None, unique_by=None):
+    return strats.lists(vector_strat, min_size=min_length, max_size=max_length, unique_by=unique_by)
+
+
+@given(vl=list_of_vectors(min_length=2, unique_by=lambda v: len(v)))
 def test_add_vectors_of_different_size_raises_exception(vl):
     with pytest.raises(ValueError):
         add(*vl)
 
 
-pairs_of_vectors = st.tuples(vectors, vectors)
-pairs_of_vectors_with_different_length = pairs_of_vectors.filter(
-    lambda pair: len(pair[0]) != len(pair[1])
-)
+two_vectors_of_different_size = list_of_vectors(
+    min_length=2, max_length=2, unique_by=lambda v: len(v))
 
 
-@given(vs=pairs_of_vectors_with_different_length)
+@given(vs=two_vectors_of_different_size)
 def test_subtract_vectors_of_different_size_raises_exception(vs):
     with pytest.raises(ValueError):
         subtract(vs[0], vs[1])
 
 
-@given(vs=pairs_of_vectors_with_different_length)
-def test_distance_betwwen_vectors_of_different_size_raises_exception(vs):
+@given(vs=two_vectors_of_different_size)
+def test_distance_between_vectors_of_different_size_raises_exception(vs):
     with pytest.raises(ValueError):
         distance(vs[0], vs[1])
 
 
-@given(vs=pairs_of_vectors_with_different_length)
-def test_dot_product_betwwen_vectors_of_different_size_raises_exception(vs):
+@given(vs=two_vectors_of_different_size)
+def test_dot_product_between_vectors_of_different_size_raises_exception(vs):
     with pytest.raises(ValueError):
         dot(vs[0], vs[1])
 
 
-@given(vs=pairs_of_vectors_with_different_length)
-def test_angle_betwwen_vectors_of_different_size_raises_exception(vs):
+@given(vs=two_vectors_of_different_size)
+def test_angle_between_vectors_of_different_size_raises_exception(vs):
     with pytest.raises(ValueError):
         angle(vs[0], vs[1])
 
 
-@given(vs=pairs_of_vectors.filter(lambda pair: len(pair[0]) != 3 or len(pair[1]) != 3))
-def test_cross_product_betwwen_non_3d_vectors_raises_exception(vs):
+@given(vs=list_of_vectors(vector_strat=vector(min_size=1, max_size=2), min_length=2, max_length=2))
+def test_cross_product_between_1d_and_2d_vectors_raises_exception(vs):
     with pytest.raises(ValueError):
         cross(vs[0], vs[1])
 
 
-@given(vs=pairs_of_vectors_with_different_length)
+@given(vs=list_of_vectors(vector_strat=vector(min_size=4, max_size=20), min_length=2, max_length=2))
+def test_cross_product_between_vectors_higher_than_3d_raises_exception(vs):
+    with pytest.raises(ValueError):
+        cross(vs[0], vs[1])
+
+
+@given(vs=two_vectors_of_different_size)
 def test_projection_length_between_vectors_of_different_size_raises_exception(vs):
     with pytest.raises(ValueError):
         projection_length(vs[0], vs[1])
 
 
-@given(v=vectors.filter(lambda v: len(v) == 3))
-def test_projection_length_raises_exsception_when_projecting_to_nul_vector(v):
+@given(v=vector(min_size=3, max_size=3))
+def test_projection_length_raises_exception_when_projecting_to_nul_vector(v):
     with pytest.raises(ValueError):
         projection_length(v, (0, 0, 0))
 
 
-@given(vl=lists_of_vectors_of_unique_length.filter(lambda vl: 3 not in map(len, vl)))
-def test_project_vectors_to_xy_plane_raises_exception_for_non_3d_vectors(vl):
+@given(vl=list_of_vectors(vector_strat=vector(min_size=1, max_size=2), min_length=1))
+def test_project_vectors_to_xy_plane_raises_exception_for_1d_and_2d_vectors(vl):
     with pytest.raises(ValueError):
         project_vectors_to_xy_plane(*vl)
 
 
-lists_of_vectors_of_equal_length = st.integers(
-    min_value=1, max_value=10, ).flatmap(
-        lambda n: st.lists(
-            st.tuples(*[scalars for _ in range(n)]), min_size=1, max_size=100)
-    )
-scalars_and_vectors_of_different_size = st.tuples(
-    st.lists(scalars), lists_of_vectors_of_equal_length).filter(
-        lambda pair: len(pair[0]) != len(pair[1])
-    )
-
-
-@ given(sv_pair=scalars_and_vectors_of_different_size)
-def test_linear_combination_raises_exception_when_number_of_scalars_is_not_equal_to_number_of_vectors(sv_pair):
+@given(vl=list_of_vectors(vector_strat=vector(min_size=4, max_size=20), min_length=1))
+def test_project_vectors_to_xy_plane_raises_exception_for_vectors_higher_than_3d(vl):
     with pytest.raises(ValueError):
-        linear_combination(sv_pair[0], *sv_pair[1])
+        project_vectors_to_xy_plane(*vl)
 
 
-scalars_and_vectors_of_unique_length = st.tuples(
-    st.lists(scalars), lists_of_vectors_of_unique_length).filter(
-        lambda pair: (len(pair[0]) == len(pair[1]))
-    )
+@composite
+def list_of_vectors_of_equal_length(draw, max_vector_size=20, min_length=0, max_length=None, unique_by=None):
+    vector_size = draw(strats.integers(min_value=0, max_value=max_vector_size))
+    vector_list = list_of_vectors(vector_strat=vector(
+        min_size=vector_size, max_size=vector_size), min_length=min_length, max_length=max_length, unique_by=unique_by)
+    return draw(vector_list)
 
 
-@ given(sv_pair=scalars_and_vectors_of_unique_length)
-def test_linear_combination_for_vectors_of_different_length_raises_exception(sv_pair):
+@ given(strats.data())
+def test_linear_combination_raises_exception_when_number_of_scalars_is_not_equal_to_number_of_vectors(data):
+    sl = data.draw(strats.lists(scalar))
+    vl = data.draw(list_of_vectors_of_equal_length(min_length=len(sl)+1))
     with pytest.raises(ValueError):
-        linear_combination(sv_pair[0], *sv_pair[1])
+        linear_combination(sl, *vl)
+
+
+@given(strats.data())
+def test_linear_combination_for_vectors_of_different_length_raises_exception(data):
+    sl = data.draw(strats.lists(scalar, min_size=2))
+    vl = data.draw(list_of_vectors(min_length=len(
+        sl), max_length=len(sl), unique_by=lambda v: len(v)))
+    with pytest.raises(ValueError):
+        linear_combination(sl, *vl)
